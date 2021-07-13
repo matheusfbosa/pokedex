520060ed01c6ae9f6168dab336dc23d3fdb1bc1a
Matheus Bosa <matheusbozza@gmail.com>
2021-07-13 20:21:16 -0300

    feat: :sparkles: Implements navigation on pages and styles

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,6 +1,8 @@
 <svelte:head>
-  <title>Pokédex | About</title>
+	<title>Pokédex | About</title>
 </svelte:head>
 
-<h1>About</h1>
-<p>This is a Pokédex built with Svelte Kit that uses Pokémon API.</p>
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p class="text-center">
+  This is a Pokédex built with Svelte Kit that uses Pokémon API.
+</p>
