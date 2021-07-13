2b7b727cf6fca8fcca5d50bc720809ce22348d7c
Matheus Bosa <matheusbozza@gmail.com>
2021-07-13 10:49:01 -0300

    feat: :art: Creating Pokestore

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,15 @@
 <script>
+  import { pokemon } from "../stores/pokestore";
 </script>
 
 <svelte:head>
   <title>Pokédex | Home</title>
 </svelte:head>
-
 <h1>Pokédex</h1>
+
+{#each $pokemon as pokeman}
+<p>{pokeman.name}</p>
+{/each}
 
 <style>
 </style>
