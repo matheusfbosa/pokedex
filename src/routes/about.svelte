a17b10aa750fa83a66dde526125d4792844ecfdb
Matheus Bosa <matheusbozza@gmail.com>
2021-07-13 23:21:32 -0300

    style: :art: Format and lint

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,6 +3,4 @@
 </svelte:head>
 
 <h1 class="text-4xl text-center my-8 uppercase">About</h1>
-<p class="text-center">
-  This is a Pokédex built with Svelte Kit that uses Pokémon API.
-</p>
+<p class="text-center">This is a Pokédex built with Svelte Kit that uses Pokémon API.</p>
