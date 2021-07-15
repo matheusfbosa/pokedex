6efeea2dd0c2555ec19e18446900e54abd49cba7
Matheus Bosa <matheusbozza@gmail.com>
2021-07-14 22:52:48 -0300

    fix: :bug: Fix search by term capital letters

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -28,7 +28,7 @@
 	$: {
 		if (searchTerm) {
 			filteredPokemon = pokemon.filter((pokeman) =>
-				pokeman.name.toLowerCase().includes(searchTerm)
+				pokeman.name.toLowerCase().includes(searchTerm.toLowerCase())
 			);
 		} else {
 			filteredPokemon = [...pokemon];
